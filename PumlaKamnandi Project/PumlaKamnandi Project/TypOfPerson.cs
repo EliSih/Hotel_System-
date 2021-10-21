c6817f03ada6ab7674d65398512f12b4c4cbb583
Lukhaya <JNTAMA001@myuct.ac.za>
2021-10-22 00:26:51 +0200

    Added classes

---
+++ PumlaKamnandi Project/PumlaKamnandi Project/TypOfPerson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumlaKamnandi_Project.Data
+{
+    class TypeOfPerson
+    {
+
+        public enum PersonType { 
+            Customer,
+            Employee
+        }
+        protected PersonType personType;
+        #region Property Methods
+        protected PersonType getPersonType
+        {
+            get { return personType; }
+            set { personType = value; }
+        
+        }
+
+
+        #endregion
+        public TypeOfPerson() {
+            personType = PersonType.Customer;
+        }
+        public PersonType GetPersonType {
+            get { return personType; }
+            set { personType = value; }
+        }
+    }
+}
