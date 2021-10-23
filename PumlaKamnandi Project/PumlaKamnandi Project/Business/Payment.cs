dd7d1d28fe18514cbc5f69224c819819c7f8c24d
Seluleko Sabela <SBLSEL002@myuct.ac.za>
2021-10-23 04:37:53 +0200

    Added Room, Booking, Payment and Invoice Classes as per DesignClassDiagram's structure.

---
+++ PumlaKamnandi Project/PumlaKamnandi Project/Business/Payment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumlaKamnandi_Project.Business
+{
+    class Payment
+    {
+        #region Data Members
+        public int ID;
+        public string type;
+        public string description;
+        public string date;
+        #endregion
+
+        #region Constructor 
+        public Payment(String date)
+        {
+            this.date = date;
+        }
+        #endregion
+
+        #region Methods (toString)
+        public string toString()
+        {
+            return ID.ToString() + " " + type + " " + description + " " + date;
+        }
+        #endregion
+    }
+}
---
+++ PumlaKamnandi Project/PumlaKamnandi Project/Business/Room.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumlaKamnandi_Project.Business
+{
+    class Room
+    {
+        #region Data members
+        private int roomNumber;
+        private decimal price;
+        private string description;
+        private int capacity;
+        #endregion
+
+        #region Access Modifiers 
+        public int getRoomNumber
+        {
+            get { return roomNumber; }
+            set { roomNumber = value; }
+        }
+
+        public decimal getPrice
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string getDescription
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public int getCapacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Room()
+        {
+
+        }
+        #endregion
+    }
+}
