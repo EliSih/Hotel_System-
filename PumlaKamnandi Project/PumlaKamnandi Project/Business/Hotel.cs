70821528c5b88d2f801a8ceac28b573f13f73a87
EliSih <SHLMAG002@myuct.ac.za>
2021-10-23 22:23:43 +0200

    added hotel class and added customer account attributes  to customer account

---
+++ PumlaKamnandi Project/PumlaKamnandi Project/Business/Hotel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumlaKamnandi_Project.Business
+{
+    class Hotel
+    {
+        #region fields
+        private int hotelID;
+        private string location;
+        private string contactDetails;
+        #endregion
+
+        #region contructor
+        public Hotel()
+        {
+
+        }
+        #endregion 
+
+        #region methods 
+        public int HotelID
+        {
+            get { return hotelID; }
+            set { hotelID = value; }
+        }
+
+        public string Location
+        {
+            get { return location;  }
+            set { location = value; }
+        }
+
+        public string ContactDetails
+        {
+            get { return contactDetails; }
+            set { contactDetails = value; }
+        }
+        #endregion 
+    }
+}
--- PumlaKamnandi Project/PumlaKamnandi Project/Data/PhumlaKamnandiDatabase.cs
+++ PumlaKamnandi Project/PumlaKamnandi Project/Data/PhumlaKamnandiDatabase.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
+using PumlaKamnandi_Project.Business; 
 
 namespace PumlaKamnandi_Project.Data
 {
